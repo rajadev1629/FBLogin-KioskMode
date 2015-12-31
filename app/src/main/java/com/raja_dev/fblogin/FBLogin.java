627a2056f04b41f5d8223be785a14106b052b9f4
Raja_dev <Raja_dev>
2015-12-31 14:33:14 +0530

    Adding working code for facebook login

--- app/src/main/java/com/raja_dev/fblogin/FBLogin.java
+++ app/src/main/java/com/raja_dev/fblogin/FBLogin.java
@@ -1,117 +1,117 @@
-//package com.raja_dev.fblogin;
-//
-//import android.app.Activity;
-//import android.net.Uri;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.widget.TextView;
-//
-//import com.facebook.AccessToken;
-//import com.facebook.CallbackManager;
-//import com.facebook.FacebookCallback;
-//import com.facebook.FacebookException;
-//import com.facebook.FacebookSdk;
-//import com.facebook.login.LoginResult;
-//import com.facebook.login.widget.LoginButton;
-//import com.google.android.gms.appindexing.Action;
-//import com.google.android.gms.appindexing.AppIndex;
-//import com.google.android.gms.common.api.GoogleApiClient;
-//import com.pkmmte.view.CircularImageView;
-//
-///**
-// * Created by User on 12/29/2015.
-// */
-//public class FBLogin extends Activity {
-//    CircularImageView mCircularImageView;
-//    TextView txtName, txtDOB;
-//    LoginButton mLoginButton;
-//    CallbackManager mCallbackManager;
-//    private static String logString = "FACEBOOK LOGIN :: ";
-//    /**
-//     * ATTENTION: This was auto-generated to implement the App Indexing API.
-//     * See https://g.co/AppIndexing/AndroidStudio for more information.
-//     */
-//    private GoogleApiClient client;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.facebook_login);
-//        //Initialise facebook SDK
-//        FacebookSdk.sdkInitialize(getApplicationContext());
-//        mCallbackManager = CallbackManager.Factory.create();
-//
-//        mCircularImageView = (CircularImageView) findViewById(R.id.imageView);
-//        txtName = (TextView) findViewById(R.id.txt_name);
-//        txtDOB = (TextView) findViewById(R.id.txt_dob);
-//        txtName.setText("this is sample text");
-//        mLoginButton = (LoginButton) findViewById(R.id.login_button);
-//        mLoginButton.setReadPermissions("user_friends");
-//
-//        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                AccessToken accessToken = loginResult.getAccessToken();
-//                Log.v(logString, "Successfull login");
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                Log.v(logString, "Cancel login");
-//            }
-//
-//            @Override
-//            public void onError(FacebookException error) {
-//                Log.e(logString, "we are getting error");
-//                error.printStackTrace();
-//
-//            }
-//        });
-//
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-//    }
-//
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "FBLogin Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.raja_dev.fblogin/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.start(client, viewAction);
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "FBLogin Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.raja_dev.fblogin/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.end(client, viewAction);
-//        client.disconnect();
-//    }
-//}
+package com.raja_dev.fblogin;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.pkmmte.view.CircularImageView;
+
+/**
+ * Created by User on 12/29/2015.
+ */
+public class FBLogin extends Activity {
+    CircularImageView mCircularImageView;
+    TextView txtName, txtDOB;
+    LoginButton mLoginButton;
+    CallbackManager mCallbackManager;
+    private static String logString = "FACEBOOK LOGIN :: ";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_login);
+        //Initialise facebook SDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+
+        mCircularImageView = (CircularImageView) findViewById(R.id.imageView);
+        txtName = (TextView) findViewById(R.id.txt_name);
+        txtDOB = (TextView) findViewById(R.id.txt_dob);
+        txtName.setText("this is sample text");
+        mLoginButton = (LoginButton) findViewById(R.id.login_button);
+        mLoginButton.setReadPermissions("user_friends");
+
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Log.v(logString, "Successfull login");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v(logString, "Cancel login");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e(logString, "we are getting error");
+                error.printStackTrace();
+
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FBLogin Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.raja_dev.fblogin/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FBLogin Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.raja_dev.fblogin/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
